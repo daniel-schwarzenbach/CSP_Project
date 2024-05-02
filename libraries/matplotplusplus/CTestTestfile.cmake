2a85354eabfb56de253a6bc01f95373409a31e7b
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2024-05-02 16:52:19 +0200

    added euler compiling

---
+++ libraries/matplotplusplus/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /cluster/home/dschwarze/CSP_Project/libraries/matplotplusplus
+# Build directory: /cluster/home/dschwarze/CSP_Project/libraries/matplotplusplus
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("source")
---
+++ libraries/matplotplusplus/source/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cluster/home/dschwarze/CSP_Project/libraries/matplotplusplus/source
+# Build directory: /cluster/home/dschwarze/CSP_Project/libraries/matplotplusplus/source
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("3rd_party")
+subdirs("matplot")
