a7685b6610fd6fd3656283f5f244375743d66ad0
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2024-05-03 14:21:01 +0200

    change euler modules

--- docs/Euler/log-in.sh
+++ docs/Euler/log-in.sh
@@ -3,7 +3,7 @@ ssh -Y dschwarze@euler.ethz.ch
 #New software stack
 env2lmod
 
-module load gcc/11.4.0 openmpi/4.1.4 eigen/3.4.0 cmake/3.26.3 openblas/0.3.20 fftw/3.3.10
+module load gcc/11.4.0 eigen/3.4.0 cmake/3.26.3 openblas/0.3.20 fftw/3.3.10  cuda/12.1.1 openmpi/4.1.4
 #sbatch jobscript.sh
 
 # module list
