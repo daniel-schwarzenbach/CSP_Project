693a98d96d6e368b68f0eb4bbcd81752b1856bee
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2024-05-04 23:46:02 +0200

    added tes

--- docs/Euler/launch2.sh
+++ docs/Euler/launch2.sh
@@ -8,5 +8,5 @@
 #SBATCH --output=output.txt
 #SBATCH --time=04:00:00
 
-mpirun -bind-to core -map-by core -n 48 .././programs/HeisenbergMPI 1.0
-mpirun -bind-to core -map-by core -n 48 .././programs/HeisenbergMPI 2.0
+mpirun -bind-to core -map-by core -n 48 .././programs/HeisenbergMPITest 1.0
+mpirun -bind-to core -map-by core -n 48 .././programs/HeisenbergMPITest 2.0
