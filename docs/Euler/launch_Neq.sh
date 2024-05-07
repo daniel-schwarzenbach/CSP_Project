92b994428fa54a8d7da7522f45ca7cfd7aa8b959
Daniel Schwarzenbach <dschwarze@student.ethz.ch>
2024-05-08 00:14:15 +0200

    removed multiple input files

--- docs/Euler/launch_Neq.sh
+++ docs/Euler/launch_Neq.sh
@@ -10,5 +10,6 @@
 
 mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI_Neq 0.5
 mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI_Neq 1
-mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI_Neq 2
+mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI_Neq 1.8
+mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI_Neq 3.0
 mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI_Neq 10
