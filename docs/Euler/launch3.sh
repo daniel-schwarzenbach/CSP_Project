1c52fc92a06edaf315b650ed563a96be99089b8f
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2024-05-11 12:14:48 +0200

    fixed the lauchscripts

--- docs/Euler/launch3.sh
+++ docs/Euler/launch3.sh
@@ -3,10 +3,10 @@
 #SBATCH --nodes=48                   
 #SBATCH --ntasks=48              
 #SBATCH --ntasks-per-node=1 
-#SBATCH --mem-per-cpu=2000
+#SBATCH --mem-per-cpu=2500
 #SBATCH --error=error.txt
 #SBATCH --output=output.txt
-#SBATCH --time=04:00:00
+#SBATCH --time=20:00:00
 
 mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI 1.54
 mpirun -bind-to core -map-by core -n 12 .././programs/HeisenbergMPI 1.55
